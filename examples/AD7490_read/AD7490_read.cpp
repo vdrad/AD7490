dc260abacfc6593480a8fe33a43d1b502f042461
vdrad <victor.drad.g@gmail.com>
2024-11-28 21:20:17 -0300

    adds example code

---
+++ examples/AD7490_read/AD7490_read.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file  AD7490_read.cpp
+ * @brief Reads all channels from AD7490.
+ * @author Victor Gomes
+ */
+
+#include "Arduino.h"
+#include "AD7490.h"
+
+#define SLCK_PIN        41
+#define DOUT_PIN        40
+#define DIN_PIN         42
+#define CS_PIN          39
+#define CLOCK_FREQUENCY 1e7
+
+AD7490 ADC;
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.println("Initializing AD7490...");
+    ADC.setAllPins(SLCK_PIN, DOUT_PIN, DIN_PIN, CS_PIN);
+    ADC.setClockFrequency(CLOCK_FREQUENCY);
+    ADC.begin();
+    /* Alternatively, you could do:
+    ADC.begin(
+        SLCK_PIN,
+        DOUT_PIN,
+        DIN_PIN,
+        CS_PIN,
+        CLOCK_FREQUENCY
+    ); */
+   Serial.println("AD7490 successfully configured!");
+}
+
+void loop() {
+    ADC.validate();
+    delay(2000);
+}
